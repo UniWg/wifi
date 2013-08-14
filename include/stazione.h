4205d13490a9ac003b811a5a8f5a58afa9843c94
zedpro <carlo.rimondi@studio.unibo.it>
2013-08-14 14:23:58 +0200

    inizializzazione stazioni

--- include/stazione.h
+++ include/stazione.h
@@ -43,6 +43,28 @@ station_t stazione [4];
 /* ------------------------------------------------------------------------- */
 /* Prototipi delle funzioni */
 
+void mac2str (const char* mac,char* asc);
+/* ----------------------------------------------------------------------------
+* Nome			: 
+* Descrizione	: converte il mac address dal formato formato esteso al 
+				  formato ascii a 6 byte
+* Par. Formali  :
+			- mac : indirizzo mac nel formato esteso xx:xx:xx:xx:xx:xx
+					non ci sono controlli sulla correttezza formale della stringa
+			- asc : contiene il risultato della conversione
+---------------------------------------------------------------------------- */
+
+void str2mac (char* asc,char* mac);
+/* ----------------------------------------------------------------------------
+* Nome			: 
+* Descrizione	: converte il mac address dal formato formato a 6 byte al  
+				  formato esteso
+* Par. Formali  :
+			- asc : indirizzo mac nel formato a 6 byte
+					non ci sono controlli sulla correttezza formale della stringa
+			- mac : contiene il risultato della conversione
+---------------------------------------------------------------------------- */
+
 void inizializza_stazioni (void);
 /* ----------------------------------------------------------------------------
 * Nome			: 
--- src/stazione.c
+++ src/stazione.c
@@ -5,40 +5,46 @@ const int _sta_di_stax [] = {_sta_di_sta1,_sta_di_sta2,_sta_di_sta3,_sta_di_sta4
 const char _mac_stax [][17] = {_mac_sta1,_mac_sta2,_mac_sta3,_mac_sta4};
 
 /* ------------------------------------------------------------------------- */
-/* Converte il mac address in ASCII */
-void mac2str (int sta,char* mac) {
-	int i,n,j;
+void mac2str (const char* mac,char* asc) {
+	int i,j;
+	unsigned int n;
 	char u [3];
 	
 	u [2] = 0;	/* terminatore */
-	j = 5;
-	for (i=16;i>1;i--) {
-		if ((i==16) || (i==13) || (i==10) || (i==7) || (i==4) || (i==1)) {
-			/* Prendiamo due caratteri (un numero) ... */
-			u [1] = _mac_stax [sta][i];
-			u [0] = _mac_stax [sta][i-1];
+	j = 0;
+	for (i=0;i<16;i++) {
+		if ((i==0) || (i==3) || (i==6) || (i==9) || (i==12) || (i==15)) {
+			/* Prendiamo due caratteri (un numero hex) ... */
+			u [0] = mac [i];
+			u [1] = mac [i+1];
 			/* ... li convertiamo prima in numero ... */
-			n = atoi (u);
+			sscanf (u,"%x",&n);
 			/* ... e poi in ascii */
-			mac [j] = (char) n; j--;
+			asc [j] = (char) n; j++;
 		}
 	}
 }
 
 /* ------------------------------------------------------------------------- */
+void str2mac (char* asc,char* mac) {
+	char s [18];
+
+	sprintf (s,"%.2X:%.2X:%.2X:%.2X:%.2X:%.2X",
+		(int)asc[0],(int)asc[1],(int)asc[2],(int)asc[3],(int)asc[4],(int)asc[5]);	
+	strncpy (mac,s,17);	
+}
+
+/* ------------------------------------------------------------------------- */
 void inizializza_stazioni (void) {
-	int i,j;
+	int i;
 	char* mac = (char*) malloc (sizeof (char) * 6);
 	
 	for (i=0;i<_nsta;i++) {
 		stazione [i].indice = i+1;
-		mac2str (i,mac);
-		for (j=5;j>0;j--)
-			printf ("%c",mac [j]);
-		strncpy (stazione [i].mac,mac,6);
+		mac2str (_mac_stax [i],stazione [i].mac);	
 		stazione [i].campo = _sta_di_stax [i];
 	}
-	
+
 	free (mac);
 }
 
